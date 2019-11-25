81e78707c91d96ffc329f3c02606fe958eb0fd66
Axiumin <sahaaritro21@gmail.com>
2019-11-25 14:06:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ commands = [
     ("/histogram/bins","Histogram Bins. Organizes data into bins for histogram. Use POST command to send data in json.")
 ]
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 @app.route("/")
 def home():
     return render_template("home.html",items=commands)
