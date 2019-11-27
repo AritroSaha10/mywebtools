615b01e9ba25df9c1eb74074a4685afbcda1613e
Axiumin <sahaaritro21@gmail.com>
2019-11-27 10:43:09 -0500

    Update stem_leaf.py

--- stem_leaf.py
+++ stem_leaf.py
@@ -1,18 +1,16 @@
-
-def stem_leaf(data):
-  final = {}
-
-  for x in data:
-    x_str = str(x)
-    if (len(x_str) == 1):
-      x_str = "0" + x_str
-
-    stem = x_str[:-1]
-    leaf = x_str[-1]
-
-    if (stem not in final):
-      final[int(stem)] = [int(leaf)]
-    else:
-      final[int(stem)] = final[int(stem)] + [int(leaf)]
-      
-    return final
+# get stem leaf plot in dictionary
+def stemleaf(data):
+	stem_leaf = {}
+	for x in data:
+		x_str = str(x)
+		if (len(x_str) == 1):
+			x_str = "0" + x_str
+	
+		stem = int(x_str[:-1])
+		leaf = int(x_str[-1])
+	
+		if (stem not in stem_leaf):
+			stem_leaf[stem] = [leaf]
+		else:
+			stem_leaf[stem] = stem_leaf[stem] + [leaf]
+	return stem_leaf
